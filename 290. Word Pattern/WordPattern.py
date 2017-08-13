ef328aa4f705460c37c674718e54493fd9fa257f
gaolccc <gaolccc@gmail.com>
2017-08-13 22:54:09 +0800

    Update WordPattern.py

--- 290. Word Pattern/WordPattern.py
+++ 290. Word Pattern/WordPattern.py
@@ -1,3 +1,17 @@
+'''
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+Examples:
+pattern = "abba", str = "dog cat cat dog" should return true.
+pattern = "abba", str = "dog cat cat fish" should return false.
+pattern = "aaaa", str = "dog cat cat dog" should return false.
+pattern = "abba", str = "dog dog dog dog" should return false.
+Notes:
+You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+'''
+
 class Solution(object):
     def wordPattern(self, pattern, str):
         """
@@ -18,3 +32,24 @@ class Solution(object):
                 theStr.append(dict[item])
     
             return True if theStr == str.split(' ') else False
+            
+'''
+Other's solution:
+1.
+def wordPattern(self, pattern, str):
+    s = pattern
+    t = str.split()
+    return map(s.find, s) == map(t.index, t)
+
+2. 
+def wordPattern(self, pattern, str):
+    f = lambda s: map({}.setdefault, s, range(len(s)))
+    return f(pattern) == f(str.split())
+
+3.
+def wordPattern(self, pattern, str):
+    s = pattern
+    t = str.split()
+    return len(set(zip(s, t))) == len(set(s)) == len(set(t)) and len(s) == len(t)
+
+'''
